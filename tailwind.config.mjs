3a73cb5a28e2ae4819b111a163ae364439dfe878
Alfredo Villegas <alfreddovillegas@gmail.com>
2024-02-17 23:34:28 -0400

    Porfolio inicial

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      //color
+      colors: {
+        'primary-400': '#00C8E0',
+        'primary-200': '#7DDFEE'
+      }
+    }
+  },
+  darkMode: 'class',
+  plugins: []
+};
